9ecba1d299f4e5015d16b9f47e1d62a8e25bc681
vikasgowda3007 <vikasgowda3007@gmail.com>
2025-12-11 10:15:09 -0700

    Build for GitHub Pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,35 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './core/layout/navbar/navbar.component/navbar.component';
+import { HeroComponent } from './sections/hero/hero.component/hero.component';
+import { AboutComponent } from './sections/about/about.component/about.component';
+import { ExperienceComponent } from './sections/experience/experience.component/experience.component';
+import { ProjectsComponent } from './sections/projects/projects.component/projects.component';
+import { SkillsComponent } from './sections/skills/skills.component/skills.component';
+import { ContactComponent } from './sections/contact/contact.component/contact.component';
+import { ResumeService } from './core/resume-service';
+import { Resume } from './core/models/resume/resume.module';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('vikas-resume');
+  resume: Resume;
+
+  constructor(private resumeService: ResumeService) {
+    this.resume = this.resumeService.getResume();
+  }
 }
